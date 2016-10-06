59213df17c15b63950494097d166d4ed79304f01
dylan <dylan.meeus@atmire.com>
2016-10-06 12:41:43 +0200

    fix comments

--- checker.hs
+++ checker.hs
@@ -1,11 +1,11 @@
--- haskell function to check for doubles
+-- haskell script to check for duplicate keys in a PHP i18n file.
 import Data.Char
 import Data.List
 import System.Environment
 import System.IO
 import Text.Regex
 
--- create an outputfile based on an inputfile name
+-- create an output filename based on an input filename
 outputfile i = parts !! 0 ++ "_multiples." ++ parts !! 1
     where parts = (splitRegex (mkRegex "\\.") i)
 
@@ -22,12 +22,11 @@ filterKeys ls = [(splitRegex splitRgx x)!!0 | x <- lines ls, isInfixOf "=>" x]
 countOccurance :: String -> [String] -> Int
 countOccurance x xs = length $ filter(\y -> y == x) xs
 
--- can make a tuple with three values, a triple
 countOccuranceIgnoreCase :: String -> [String] -> Int
 countOccuranceIgnoreCase x xs = length $ filter(\y -> map toLower y == lx) xs
     where lx = map toLower x
 
--- we can remove the leading spaces perhaps
+-- format tuple output
 printTuple :: (Show a, Show b, Show c) => (a,b,c) -> String
 printTuple (a,b,c) = (splitRegex  (mkRegex "'") (show a))!!1 ++ " => " ++ (show b) ++ ",  case insensitive: " ++ (show c)
 
@@ -36,5 +35,4 @@ main = do
     [i] <- getArgs
     content <- readFile i
     writeFile (outputfile i) $ intercalate "\n" (map printTuple (checkMultiple content))
--- code to write this out to a file
---
+
